60ad3e1a52d1a98011980c27bd3e0281e2232b14
Hakan Ardo <ardo@maths.lth.se>
2017-09-08 14:07:57 +0200

    2 node job

---
+++ train_cifar10_4gpu_x2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+#SBATCH -t 48:00:00
+#SBATCH -N 2
+#SBATCH -p gpu --gres=gpu:4 --mem-per-cpu=3100 -tasks-per-node=20
+#SBATCH --exclusive
+
+cp /lunarc/nobackup/projects/lu-haar/cifar10/* $SNIC_TMP
+
+export MXNET_CUDNN_AUTOTUNE_DEFAULT=1
+#export GOMP_CPU_AFFINITY="0-19:2"
+
+python launch.py --launcher mpi python -u train_resnet.py --data-dir $SNIC_TMP --data-type cifar10 --depth 164 \
+       --batch-size 128 --num-classes 10 --num-examples 50000 --epochs 20 --gpus=0,1,2,3 --kv-store dist
