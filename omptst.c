a5bdabfecc15e843972d9e00e7c7c07c69f37a61
Hakan Ardo <ardo@maths.lth.se>
2017-09-08 11:57:14 +0200

    tst

---
+++ omptst.c
@@ -0,0 +1,14 @@
+void main() {
+  int maxthread, threadget;
+  #pragma omp parallel
+  {
+    // be conservative, set number of real cores
+    maxthread = omp_get_num_procs() / 2 - 1;
+  }
+  int preprocess_threads = maxthread;
+  #pragma omp parallel num_threads(preprocess_threads)
+  {
+    threadget = omp_get_num_threads();
+  }
+  printf("%d, %d\n", maxthread, threadget);
+}
