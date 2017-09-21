9803b7096202d455110f48b066293828daf69edf
Hakan Ardo <ardo@maths.lth.se>
2017-09-21 15:28:08 +0200

    run

--- run_im2rec_ids.sh
+++ run_im2rec_ids.sh
@@ -5,4 +5,4 @@
 
 cd /lunarc/nobackup/projects/lu-haar/imagenet/
 python -u /lunarc/nobackup/users/hakanardo/miniconda2/lib/python2.7/site-packages/mxnet/tools/im2rec.py \
-    --resize 480 --quality  90 train_ids ILSVRC2012_img_train/ --num-thread 8
+    --resize 480 --quality  90 train_ids ILSVRC2012_img_train/ --num-thread 8 --pack-label 1
